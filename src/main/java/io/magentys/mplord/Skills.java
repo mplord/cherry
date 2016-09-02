764578eebf92e7d0f8d614e886231c202a10d9ba
Miles Lord <miles.lord@magentys.io>
2016-09-02 03:49:42 +0100

    Use Guice

--- src/main/java/io/magentys/mplord/Skills.java
+++ src/main/java/io/magentys/mplord/Skills.java
@@ -1,22 +1,23 @@
 package io.magentys.mplord;
 
+import com.google.inject.Provider;
+
 import io.magentys.mplord.agent.AgentTypedMemory;
 
 public class Skills {
     private AgentTypedMemory agent;
 
-    public Skills(AgentTypedMemory agent) {
+    public void withAgent(AgentTypedMemory agent) {
         this.agent = agent;
     }
 
-    protected <T extends Skill> T skill(Class<T> skillClass) {
-        try {
-            T skill = skillClass.newInstance();
-            skill.useAgent(agent);
-            return skill;
-        } catch (Exception e) {
-            // TODO hacky - fix
-        }
-        return null;
+    protected AgentTypedMemory getAgent() {
+        return agent;
+    }
+
+    protected <T extends Skill> T skill(Provider<T> skillProvider) {
+        T skill = skillProvider.get();
+        skill.useAgent(agent);
+        return skill;
     }
 }
